b9d418ba93c4fe0356346867e913d18cb549373b
JoeyPine <fanjiasong_7@outlook.com>
2020-03-22 21:15:20 +0800

    修复比较整数时出现空指针异常

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -12,6 +12,10 @@ public class Main {
     // 当且仅当它们代表的整数相等时，此方法返回true
     // 不要修改本方法参数的类型
     public static boolean compare(int a, Integer b) {
-        return a == b;
+        Integer b1 = b;
+        if(b == null){
+            b1 = 0;
+        }
+        return a == b1;
     }
 }
