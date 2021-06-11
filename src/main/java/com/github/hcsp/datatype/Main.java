84049f0ade87ec291efd6b425d34b844e46afab6
Jianqi Zhao <harris.net.newjersy@gmail.com>
2021-06-11 23:10:05 +1000

    job done

--- src/main/java/com/github/hcsp/datatype/Main.java
+++ src/main/java/com/github/hcsp/datatype/Main.java
@@ -12,6 +12,15 @@ public class Main {
     // 当且仅当它们代表的整数相等时，此方法返回true
     // 不要修改本方法参数的类型
     public static boolean compare(int a, Integer b) {
-        return a == b;
+
+        int bInt;
+        if (null == b) {
+            bInt = 0;
+        } else {
+            bInt = b.intValue();
+        }
+        return a == bInt;
     }
 }
+
+
